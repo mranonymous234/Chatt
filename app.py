3a9c7f484771660b23441e9e102992b78b05b6a9
mranonymous234 <chathu_cna@icloud.com>
2025-05-24 19:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ login_manager.login_view = 'login'
 
 # Database Models
 class User(UserMixin, db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
