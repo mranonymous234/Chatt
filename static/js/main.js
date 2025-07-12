3264f6d4a13099654d599eb0d25f8665cd041e55
mranonymous234 <chathu_cna@icloud.com>
2025-07-12 23:06:13 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -100,3 +100,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then(reg => {
+      console.log('Service Worker registered', reg);
+    })
+    .catch(err => {
+      console.error('Service Worker registration failed', err);
+    });
+}
