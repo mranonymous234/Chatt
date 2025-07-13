54188160d640ab754a0a4e10d04cd35461475419
mranonymous234 <chathu_cna@icloud.com>
2025-07-13 10:20:29 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,23 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKzhBMcMwGjml2H1bwtYwtFoW3DL7OBIg",
-    authDomain: "chart-9e5cb.firebaseapp.com",
-    projectId: "chart-9e5cb",
-    storageBucket: "chart-9e5cb.firebasestorage.app",
-    messagingSenderId: "232200875234",
-    appId: "1:232200875234:web:565ebaee263e2831e782bb",
-    measurementId: "G-VCLEVPDYZL"
+  apiKey: "AIzaSyAKzhBMcMwGjml2H1bwtYwtFoW3DL7OBIg",
+  authDomain: "chart-9e5cb.firebaseapp.com",
+  projectId: "chart-9e5cb",
+  storageBucket: "chart-9e5cb.firebasestorage.app",
+  messagingSenderId: "232200875234",
+  appId: "1:232200875234:web:565ebaee263e2831e782bb",
+  measurementId: "G-VCLEVPDYZL"
+};
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/static/images/favicon.png',
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: '/static/favicon.png' // Optional
+  });
 });
